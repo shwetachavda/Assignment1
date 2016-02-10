a6b8746e2465b8c71d07a7bb0c1c47b226eb5ded
shwetachavda <shwetachavda13@gmail.com>
2016-02-10 00:41:15 -0500

    designed and created aboutme page

--- routes/index.js
+++ routes/index.js
@@ -7,10 +7,18 @@ router.get('/', function(req, res, next) {
 });
 /* GET About Page */
 router.get('/about', function(req, res, next) {
-  res.render('index', { title: 'About Me' });
+  res.render('about', { title: 'About Me' });
 });
 /* GET Contact Page */
 router.get('/contact', function(req, res, next) {
   res.render('contact', { title: 'Contact Me' });
+});
+/* GET Projects Page */
+router.get('/projects', function(req, res, next) {
+  res.render('projects', { title: 'Projects' });
+});
+/* GET Services Page */
+router.get('/service', function(req, res, next) {
+  res.render('service', { title: 'Services' });
 });
 module.exports = router;
